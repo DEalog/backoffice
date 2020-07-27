122a41321741ad58c40ad0a748238a5952c5e81c
Flo Schuessel <mail@florian-schuessel.de>
2020-07-27 17:45:03 +0200

    Fix prod config for websockets

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :dealog_backoffice, DealogBackofficeWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: {:system, "HOSTNAME"}],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  server: true
 
 # Do not print debug messages in production
 config :logger, level: :info
