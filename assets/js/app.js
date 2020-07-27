d275606b9fe835ee5ea39e278b6734ed937ef56c
Flo Schuessel <mail@florian-schuessel.de>
2020-07-27 17:34:47 +0200

    Add live loading

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,9 +10,32 @@ import "../css/app.css";
 //     import socket from "./socket"
 //
 import "phoenix_html";
+import { Socket } from "phoenix";
+import NProgress from "nprogress";
+import { LiveSocket } from "phoenix_live_view";
 import "alpinejs";
 import resizer from "./utils/resizer";
 import controls from "./controls";
+
+let csrfToken = document
+  .querySelector("meta[name='csrf-token']")
+  .getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: { _csrf_token: csrfToken },
+});
+
+// Show progress bar on live navigation and form submits
+NProgress.configure({ showSpinner: false });
+window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
+window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
+
+// connect if there are any LiveViews on the page
+liveSocket.connect();
+
+// expose liveSocket on window for web console debug logs and latency simulation:
+// >> liveSocket.enableDebug()
+// >> liveSocket.enableLatencySim(1000)
+window.liveSocket = liveSocket;
 
 //
 // Design System
