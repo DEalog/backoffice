f4a4374dbeb79dcae2570452afef5f3fa07c5588
Flo Schuessel <mail@florian-schuessel.de>
2020-08-18 00:06:54 +0200

    Add UI form tests

--- test/dealog_backoffice_web/live/new_message_live_test.exs
+++ test/dealog_backoffice_web/live/new_message_live_test.exs
@@ -4,8 +4,23 @@ defmodule DealogBackofficeWeb.NewMessageLiveTest do
   import Phoenix.LiveViewTest
 
   test "disconnected and connected render", %{conn: conn} do
-    {:ok, page_live, disconnected_html} = live(conn, "/organization-messages/new")
-    assert disconnected_html =~ "New message"
-    assert render(page_live) =~ "New message"
+    {:ok, view, html} = live(conn, "/organization-messages/new")
+    assert html =~ "New message"
+    assert render(view) =~ "New message"
+  end
+
+  test "should render form", %{conn: conn} do
+    {:ok, view, _} = live(conn, "/organization-messages/new")
+    rendered = render(view)
+    assert rendered =~ "Title"
+    assert rendered =~ "Body"
+    assert rendered =~ "Create new message"
+  end
+
+  test "should create a new message", %{conn: conn} do
+    {:ok, view, _} = live(conn, "/organization-messages/new")
+
+    assert {_, {:live_redirect, %{to: "/organization-messages"}}} =
+             render_submit(view, :create_message, %{message: %{title: "A title", body: "A body"}})
   end
 end
