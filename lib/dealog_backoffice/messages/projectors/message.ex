4dfad9addd884f81dd9faa08f45fb14a66e15db8
Flo Schuessel <mail@florian-schuessel.de>
2020-08-17 14:56:47 +0200

    Change inserted_at to event creation date

--- lib/dealog_backoffice/messages/projectors/message.ex
+++ lib/dealog_backoffice/messages/projectors/message.ex
@@ -8,13 +8,18 @@ defmodule DealogBackoffice.Messages.Projectors.Message do
 
   alias DealogBackoffice.Messages.Projections.Message
 
-  project(%MessageCreated{} = created, fn multi ->
-    Ecto.Multi.insert(multi, :message, %Message{
-      id: created.message_id,
-      title: created.title,
-      body: created.body,
-      status: "draft"
-    })
+  project(%MessageCreated{} = created, metadata, fn multi ->
+    Ecto.Multi.insert(
+      multi,
+      :message,
+      %Message{
+        id: created.message_id,
+        title: created.title,
+        body: created.body,
+        status: created.status,
+        inserted_at: metadata.created_at
+      }
+    )
   end)
 
   project(%MessageChanged{} = changed, fn multi ->
