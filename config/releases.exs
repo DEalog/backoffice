496bd7497c6f24af3edc40a340a93c111afb6944
Flo Schuessel <mail@florian-schuessel.de>
2020-09-18 23:50:04 +0200

    Change string to atom conversion

--- config/releases.exs
+++ config/releases.exs
@@ -26,7 +26,7 @@ config :dealog_backoffice, DealogBackoffice.EventStore,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
 config :dealog_backoffice, :projection,
-  projectors: String.to_atom(System.get_env("PROJECTORS")) || :all
+  projectors: String.to_existing_atom(System.get_env("PROJECTORS")) || :all
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
