74859f4b294ebddac55e6e1cf040a33c658046a8
Flo Schuessel <scflode@users.noreply.github.com>
2020-10-02 21:08:01 +0200

    Change how import of admin areas runs (DEV-72) (#36)

--- rel/run.sh
+++ rel/run.sh
@@ -2,5 +2,6 @@
 
 bin/backoffice eval "DealogBackoffice.Release.init_event_store()"
 bin/backoffice eval "DealogBackoffice.Release.migrate()"
+bin/backoffice eval "DealogBackoffice.Release.import_administrative_areas()"
 
 bin/backoffice start
