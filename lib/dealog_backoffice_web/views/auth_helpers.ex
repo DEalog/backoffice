e84fbd39fd4dead08fc24c63f0ea8d701f9bf16e
Flo Schuessel <mail@florian-schuessel.de>
2021-03-24 20:33:02 +0100

    Fix wrongly disabled actions to be shown again (DEV-208)

--- lib/dealog_backoffice_web/views/auth_helpers.ex
+++ lib/dealog_backoffice_web/views/auth_helpers.ex
@@ -4,7 +4,7 @@ defmodule DealogBackofficeWeb.AuthHelpers do
   """
 
   def is_account_complete?(%DealogBackoffice.Accounts.User{account: nil}), do: false
-  def is_account_complete?(_), do: false
+  def is_account_complete?(_), do: true
 
   def is_account_incomplete?(%DealogBackoffice.Accounts.User{account: nil}), do: true
   def is_account_incomplete?(_), do: false
