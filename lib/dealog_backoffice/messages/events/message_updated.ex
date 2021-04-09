320e45eb527c5f0af20703a5552a03cbe0655b06
Flo Schuessel <mail@florian-schuessel.de>
2021-04-09 18:22:00 +0200

    Fix wrong upcast

--- lib/dealog_backoffice/messages/events/message_updated.ex
+++ lib/dealog_backoffice/messages/events/message_updated.ex
@@ -11,12 +11,12 @@ defmodule DealogBackoffice.Messages.Events.MessageUpdated do
   defimpl Commanded.Event.Upcaster, for: __MODULE__ do
     alias DealogBackoffice.Messages.Events.MessageUpdated
 
-    def upcast(%MessageUpdated{status: "updated", category: nil} = event, _metadata) do
-      %MessageUpdated{event | status: :updated, category: "Other"}
+    def upcast(%MessageUpdated{status: "published", category: nil} = event, _metadata) do
+      %MessageUpdated{event | status: :published, category: "Other"}
     end
 
-    def upcast(%MessageUpdated{status: "updated"} = event, _metadata) do
-      %MessageUpdated{event | status: :updated}
+    def upcast(%MessageUpdated{status: "published"} = event, _metadata) do
+      %MessageUpdated{event | status: :published}
     end
   end
 end
